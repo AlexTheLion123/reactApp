b0b3f4fb13a3c9e91264c57b30371168d12eb12b
Alex <alexurban12@gmail.com>
2021-09-16 14:40:49 +0200

    babel-loader working, removed node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist')
-        },
-        compress: true,
-        port: 3000,
-        open: true
-    }
-}
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		compress: true,
+		port: 3000,
+		open: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							cacheDirectory: true,
+						},
+					},
+				],
+			},
+		],
+	},
+};
